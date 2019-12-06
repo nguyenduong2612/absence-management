e22f791b70d813d95990aab6d2c8e633cd86ac07
nguyenduong2612 <nguyenduong261298@gmail.com>
2019-12-06 20:56:51 +0700

    first commit

---
+++ database/migrations/2019_12_06_134652_create_absences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAbsencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->text('reason');
+            $table->timestamp('start_at')->nullable();
+            $table->timestamp('end_at')->nullable();
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absences');
+    }
+}
